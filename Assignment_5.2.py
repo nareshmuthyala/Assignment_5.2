9b5ebff0aff7d7acdb5bee3c08f72eef83774aa8
Naresh Muthyala <nareshmuthyala1@gmail.com>
2018-03-10 09:55:46 +0530

    Assignment_5.2

---
+++ Assignment_5.2.py
@@ -0,0 +1,7 @@
+subjects=["Americans ","Indians"]
+verbs=["play","watch"]
+objects=["Baseball","Cricket"]
+for subject in subjects:
+    for verb in verbs:
+        for object in objects:
+            print(subject+" "+verb+" "+object)
